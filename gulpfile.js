ce0802db17a8dbdb82f1140ea71b6b09cbf66281
Vika <viktoriag373@gmail.com>
2021-08-04 17:38:13 +0300

    1. Разработка слайдера с ползунком 2. Некоторые общие доработки

--- gulpfile.js
+++ gulpfile.js
@@ -196,17 +196,17 @@ function jsWatch(cb) {
 
 function images(cb) {
 	return src(path.src.images)
-		// .pipe(imagemin([
-		// 	imagemin.gifsicle({interlaced: true}),
-		// 	imagemin.mozjpeg({quality: 95, progressive: true}),
-		// 	imagemin.optipng({optimizationLevel: 5}),
-		// 	imagemin.svgo({
-		// 		plugins: [
-		// 			{ removeViewBox: true },
-		// 			{ cleanupIDs: false }
-		// 		]
-		// 	})
-		// ]))
+		.pipe(imagemin([
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.mozjpeg({quality: 95, progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{ removeViewBox: true },
+					{ cleanupIDs: false }
+				]
+			})
+		]))
 		.pipe(dest(path.build.images))
 		.pipe(browserSync.reload({stream: true}));
 
